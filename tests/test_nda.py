f5b2f4eb9bc13b781b8abcc89dfce96b85fac2a6
yihao6 <yihao6.2018@gmail.com>
2023-11-27 14:16:58 -0500

    Update tests

--- tests/test_nda.py
+++ tests/test_nda.py
@@ -1,84 +1,73 @@
 import numpy as np
 import argparse
-import pdb
 import nibabel as nib
+import torch
 
-from digital_diffeomorphism.jacobian_calculator import calc_Ji_3d, calc_Ji_2d
-from digital_diffeomorphism.kernel_creator import KernelCreator
+from digital_diffeomorphism.jacobian_calculator_torch import calc_jacdets_2d
 from digital_diffeomorphism.utils import identity_grid_like, identity_grid, spatial_padding
 from digital_diffeomorphism.non_diffeomorphic_measures import calc_non_diffeomorphic_area
 
 class TestExample():
-    def example_setup(self):
+    def setup_method(self, method):
         transformation = identity_grid(shape=[3, 3])
         transformation[1, 1, :] = [0, 2]
-        transformation = spatial_padding(transformation)
 
-        self.jacobian_determinants = {}
-        kernel_creator = KernelCreator(ndim=2)
-        for diff_direction in ['00', '++', '+-', '-+', '--']:
-            kernels = kernel_creator.create_kernels(diff_direction)
-            self.jacobian_determinants[diff_direction] = calc_Ji_2d(transformation, kernels)
+        self.jacdets = calc_jacdets_2d(transformation, torch.device("cpu"))
 
     def test_central_difference(self):
         target = np.array([[1, 0.5, 1], [1.5, 1, 0.5], [1, 1.5, 1]])
-        assert np.array_equal(self.jacobian_determinants['00'], target)
+        assert np.array_equal(self.jacdets['00'], target)
 
     def test_forward_forward_difference(self):
         target = np.array([[1, 0, 1], [2, 1, 1], [1, 1, 1]])
-        assert np.array_equal(self.jacobian_determinants['++'], target)
+        assert np.array_equal(self.jacdets['++'], target)
 
     def test_forward_backward_difference(self):
         target = np.array([[1, 0, 1], [1, 3, 0], [1, 1, 1]])
-        assert np.array_equal(self.jacobian_determinants['+-'], target)
+        assert np.array_equal(self.jacdets['+-'], target)
 
     def test_backward_forward_difference(self):
         target = np.array([[1, 1, 1], [2, -1, 1], [1, 2, 1]])
-        assert np.array_equal(self.jacobian_determinants['-+'], target)
+        assert np.array_equal(self.jacdets['-+'], target)
 
     def test_backward_backward_difference(self):
         target = np.array([[1, 1, 1], [1, 1, 0], [1, 2, 1]])
-        assert np.array_equal(self.jacobian_determinants['--'], target)
+        assert np.array_equal(self.jacdets['--'], target)
 
     def test_non_diffeomorphic_area(self):
         mask = np.ones(shape=[3, 3])
-        non_diff_area = calc_non_diffeomorphic_area(self.jacobian_determinants, mask)
+        non_diff_area = calc_non_diffeomorphic_area(self.jacdets, mask)
         target = np.array([[0, 0, 0], [0, 1/4, 0], [0, 0, 0]])
         assert np.array_equal(non_diff_area, target)
 
 class TestIdentity:
-    def identity_setup(self):
+    def setup_method(self, method):
         transformation = identity_grid(shape=[3, 3])
-        transformation = spatial_padding(transformation)
 
-        self.jacobian_determinants = {}
-        kernel_creator = KernelCreator(ndim=2)
-        for diff_direction in ['00', '++', '+-', '-+', '--']:
-            kernels = kernel_creator.create_kernels(diff_direction)
-            self.jacobian_determinants[diff_direction] = calc_Ji_2d(transformation, kernels)
+        self.jacdets = calc_jacdets_2d(transformation, torch.device("cpu"))
 
     def test_central_difference(self):
         target = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-        assert np.array_equal(self.jacobian_determinants['00'], target)
+        assert np.array_equal(self.jacdets['00'], target)
 
     def test_forward_forward_difference(self):
         target = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-        assert np.array_equal(self.jacobian_determinants['++'], target)
+        assert np.array_equal(self.jacdets['++'], target)
 
     def test_forward_backward_difference(self):
         target = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-        assert np.array_equal(self.jacobian_determinants['+-'], target)
+        assert np.array_equal(self.jacdets['+-'], target)
 
     def test_backward_forward_difference(self):
         target = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-        assert np.array_equal(self.jacobian_determinants['-+'], target)
+        assert np.array_equal(self.jacdets['-+'], target)
 
     def test_backward_backward_difference(self):
         target = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-        assert np.array_equal(self.jacobian_determinants['--'], target)
+        assert np.array_equal(self.jacdets['--'], target)
 
     def test_non_diffeomorphic_area(self):
         mask = np.ones(shape=[3, 3])
-        non_diff_area = calc_non_diffeomorphic_area(self.jacobian_determinants, mask)
+        non_diff_area = calc_non_diffeomorphic_area(self.jacdets, mask)
         target = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
         assert np.array_equal(non_diff_area, target)
--- tests/test_proposition_5.py
+++ tests/test_proposition_5.py
@@ -1,10 +1,12 @@
 import numpy as np
 import argparse
-import pdb
 import nibabel as nib
+import torch
 
-from digital_diffeomorphism.jacobian_calculator import calc_Ji_3d, calc_Ji_2d
-from digital_diffeomorphism.kernel_creator import KernelCreator
+from digital_diffeomorphism.jacobian_calculator_torch import (
+            calc_jacdets_3d,
+            calc_jacdets_2d
+)
 from digital_diffeomorphism.utils import identity_grid_like
 
 def test_proposition_5_ndv():
@@ -12,35 +14,27 @@ def test_proposition_5_ndv():
     displacement_field = np.random.uniform(low=-10, high=10, size=(100, 100, 100, 3))
     transformation = displacement_field + identity_grid_like(displacement_field)
 
-    jacobian_determinants = {}
-    kernel_creator = KernelCreator(ndim=3)
     # calculate the finite difference based jacobian determinants
-    for diff_direction in ['000', '+++', '++-', '+-+', '+--', '-++', '-+-', '--+', '---']:
-        kernels = kernel_creator.create_kernels(diff_direction)
-        jacobian_determinants[diff_direction] = calc_Ji_3d(transformation, kernels)
+    jacdets = calc_jacdets_3d(transformation, torch.device("cpu"))
 
-    jacobian_determinants['Ji_intersection'] = np.ones_like(jacobian_determinants['000'])
-    for diff_direction in ['+++', '++-', '+-+', '+--', '-++', '-+-', '--+', '---']:
-        jacobian_determinants['Ji_intersection'] *= (jacobian_determinants[diff_direction] > 0)
+    jacdets['intersection'] = np.ones_like(jacdets['000'])
+    for direction in ['+++', '++-', '+-+', '+--', '-++', '-+-', '--+', '---']:
+        jacdets['intersection'] *= (jacdets[direction] > 0)
 
-    # if a voxel has 'Ji_intersection' positive, the central difference must be positive
-    assert np.sum((jacobian_determinants['Ji_intersection'] > 0) * (jacobian_determinants['000'] <= 0)) == 0
+    # if a voxel has 'intersection' positive, the central difference must be positive
+    assert np.sum((jacdets['intersection'] > 0) * (jacdets['000'] <= 0)) == 0
 
 def test_proposition_5_nda():
     # Generate a random 1000x1000x2 matrix with values in the range [-5, 5]
     displacement_field = np.random.uniform(low=-5, high=5, size=(1000, 1000, 2))
     transformation = displacement_field + identity_grid_like(displacement_field)
 
-    jacobian_determinants = {}
-    kernel_creator = KernelCreator(ndim=2)
     # calculate the finite difference based jacobian determinants
-    for diff_direction in ['00', '++', '+-', '-+', '--']:
-        kernels = kernel_creator.create_kernels(diff_direction)
-        jacobian_determinants[diff_direction] = calc_Ji_2d(transformation, kernels)
+    jacdets = calc_jacdets_2d(transformation, torch.device("cpu"))
 
-    jacobian_determinants['Ji_intersection'] = np.ones_like(jacobian_determinants['00'])
-    for diff_direction in ['++', '+-', '-+', '--']:
-        jacobian_determinants['Ji_intersection'] *= (jacobian_determinants[diff_direction] > 0)
+    jacdets['intersection'] = np.ones_like(jacdets['00'])
+    for direction in ['++', '+-', '-+', '--']:
+        jacdets['intersection'] *= (jacdets[direction] > 0)
 
-    # if a voxel has 'Ji_intersection' positive, the central difference must be positive
-    assert np.sum((jacobian_determinants['Ji_intersection'] > 0) * (jacobian_determinants['00'] <= 0)) == 0
+    # if a voxel has 'intersection' positive, the central difference must be positive
+    assert np.sum((jacdets['intersection'] > 0) * (jacdets['00'] <= 0)) == 0
